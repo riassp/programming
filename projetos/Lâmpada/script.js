c8f79b634dcd21377ad812dfc2f5e90ede419ce7
riassp <senpaisla374@gmail.com>
2024-01-06 12:36:05 -0300

    final

--- projetos/Lâmpada/script.js
+++ projetos/Lâmpada/script.js
@@ -1,4 +1,4 @@
-
+var LampadaQuebrada = false;
 
 function double(){
     let img = document.querySelector('#img')
@@ -7,23 +7,25 @@ function double(){
     let botaoD = document.querySelector('#botaoDesligar')
     botaoL.disabled = true;
     botaoD.disabled = true;
+    LampadaQuebrada = true;
 }
 
 
 function ligar(){
-
     let img = document.querySelector('#img')
     img.src = "images/ligada.jpg"
 }
 
-function hover1(){
-    let img = document.querySelector('#img')
-    img.src = "images/ligada.jpg"
+function secundaria(){
+    if(!LampadaQuebrada){
+         document.getElementById("img").src = "images/ligada.jpg";
+    }
 }
 
-function hover2(){
-    let img = document.querySelector('#img')
-    img.src = "images/desligada.jpg"
+function original(){
+    if(!LampadaQuebrada){
+        document.getElementById("img").src = "images/desligada.jpg";
+    }
 }
 
 function desligar(){
